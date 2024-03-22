7c92b2f07c2264cd2d1ceb868a49402c260ea8c3
Ronit Aryan <shadowsouledx@gmail.com>
2024-03-22 16:41:06 +0530

    made progress on unet

--- unet.py
+++ unet.py
@@ -56,5 +56,64 @@ class UNET_ResidualBlock(nn.Module):
         return merged
 
 class UNET_AttentionBlock(nn.Module):
-    def __init__(self, n_head, n_embed, d_context=768) -> None:
+    def __init__(self, n_heads, n_embed, d_context=768) -> None:
         super().__init__()
+        channels = n_heads * n_embed
+        
+        self.groupnorm = nn.GroupNorm(32, channels, eps=1e-6)
+        self.conv_input = nn.Conv2d(channels, channels, kernel_size=1, padding=0)
+
+        self.layernorm_1 = nn.LayerNorm(channels)
+        self.attention_1 = SelfAttention(n_heads, channels, in_proj_bias=False)
+        self.layernorm_2 = nn.LayerNorm(channels)
+        self.attention_2 = CrossAttention(n_heads, channels, d_context, in_proj_bias=False)
+        self.layernorm_3 = nn.LayerNorm(channels)
+        self.linear_geglu_1 = nn.Linear(channels, channels, 4 * channels * 2)
+        self.linear_geglu_2 = nn.Linear(4 * channels, channels)
+
+        self.conv_output = nn.Conv2d(channels, channels, kernel_size=1, padding=0)
+
+    def forward(self, x, context):
+        # x: (Batch, channels, height, width)
+        # context: (B, seqlen, dim)
+        residue_long = x
+        x = self.groupnorm(x)
+        x = self.conv_input(x)
+        b, c, h, w = x.shape
+        x = x.view((b, c, h*w))
+        x = x.transpose(-1,-2)
+        # x: (b, h * w, c)
+
+        residue_short = x
+        x = self.layernorm_1(x)
+        x = self.attention_1(x)
+        x += residue_short
+
+        residue_short = x
+        x = self.layernorm_2(x)
+        x = self.attention_2(x)
+        x += residue_short
+
+        residue_short = x
+        x = self.layernorm_3(x)
+        x, gate = self.linear_geglu_1(x).chunk(2, dim=-1)
+        x = x * F.gelu(gate)
+        x = self.linear_geglu_2(x)
+        x += residue_short
+
+        x = x.transpose(-1,-2)
+        x.view((b, c, h, w))
+        x = self.conv_output(x) + residue_long
+        return x
+    
+
+class Upsample(nn.Module):
+    def __init(self, channels):
+        super().__init__()
+        self.conv = nn.Conv2(channels, channels, kernel_size=3, padding=1)
+    
+    def forward(self, x):
+        # x: (B, C, H, W)
+        x = F.interpolate(x,scale_factor=2, mode='nearest')
+        # x: (B, C, 2H, 2W)
+        return self.conv(x)
